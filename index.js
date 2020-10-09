322b8f95992730b1074dd66d9a41a81716896f0d
Reuben Reyes <reuben.a.reyes@gmail.com>
2020-10-08 22:48:18 -0700

    !paco

--- index.js
+++ index.js
@@ -9,13 +9,14 @@ const { logger } = config;
 
 const discord = new Discord.Client();
 
-DEBOUNCE_WAIT_TIME = 30000 // ms
+RANDOM_DEBOUNCE_WAIT_TIME = 30000 // ms
+COMMAND_DEBOUNCE_WAIT_TIME = 1000 // ms
 
-function getPhotoPoster(channel) {
+function getPhotoPoster(channel, probability = 0.5) {
     return async function postIanPhoto() {
         logger.info('Rolling D2');
 
-        if (Math.random() >= 0.5) {
+        if (Math.random() >= 1 - probability) {
             logger.info('Rolled 2, Ian photo coming up');
 
             const photo = await fetch('https://t1kt4p30ca.execute-api.us-west-2.amazonaws.com/v1/picture')
@@ -42,19 +43,32 @@ discord.on('ready', async () => {
 
 })
 
-let postIanPhoto = null;
+let randomlyPostIanPhoto = null;
+let alwaysPostIanPhoto = null;
 
 discord.on('message', (msg) => {
-    if (msg.author.id === '187657955422765062') {
-        if (!postIanPhoto) {
+    if (msg.content === '!paco') {
+        if (!alwaysPostIanPhoto) {
             logger.info('Photo poster not initialized, initializing');
-            photoPoster = getPhotoPoster(msg.channel);
+            const photoPoster = getPhotoPoster(msg.channel, 1);
 
             logger.info('Initialized, throttling');
-            postIanPhoto = throttle(photoPoster, 30000);
+            randomlyPostIanPhoto = throttle(photoPoster, COMMAND_DEBOUNCE_WAIT_TIME);
         }
 
-        postIanPhoto();
+       return randomlyPostIanPhoto();
+    }
+
+    if (msg.author.id === '187657955422765062') {
+        if (!randomlyPostIanPhoto) {
+            logger.info('Random photo poster not initialized, initializing');
+            const photoPoster = getPhotoPoster(msg.channel, 0.5);
+
+            logger.info('Initialized, throttling');
+            randomlyPostIanPhoto = throttle(photoPoster, RANDOM_DEBOUNCE_WAIT_TIME);
+        }
+
+       return randomlyPostIanPhoto();
     }
 })
 
